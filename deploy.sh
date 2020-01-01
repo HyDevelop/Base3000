f94e519f2ab219c552050f5ec5be7b510f80bbc0
Hykilpikonna <admin@moecraft.cc>
2020-01-01 17:42:44 -0500

    update

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'b3k.hydev.org' > CNAME
+
+git add -A
+git commit -m 'update'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:HyDevelop/Base3000.git master:gh-pages
+
+cd -
