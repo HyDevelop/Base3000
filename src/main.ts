29d25fa94dc099e0bd16807a6a6512fc1e6518da
Hykilpikonna <admin@moecraft.cc>
2020-01-01 15:28:52 -0500

    [+] Activate ElementUI

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+import ElementUI from 'element-ui';
+Vue.use(ElementUI);
+
 new Vue({
     render: (h) => h(App),
 }).$mount('#app');
