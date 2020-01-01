a3c23816010bd0f929674bf108a50bb71602dbc0
Hykilpikonna <admin@moecraft.cc>
2020-01-01 15:21:46 -0500

    [+] Import freqList

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-    import HelloWorld from './components/HelloWorld.vue';
+
+    import freqList from '@/freq-list-zh.json'
 
     @Component
     export default class App extends Vue
